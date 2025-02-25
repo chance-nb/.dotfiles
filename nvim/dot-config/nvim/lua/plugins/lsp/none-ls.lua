437ef091276b63c467a854d02cce602c8e8c39cc
astrid <--list>
2025-02-25 22:59:25 +0100

    nvim: add clang_format

--- nvim/dot-config/nvim/lua/plugins/lsp/none-ls.lua
+++ nvim/dot-config/nvim/lua/plugins/lsp/none-ls.lua
@@ -9,7 +9,8 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.goimports
+				null_ls.builtins.formatting.goimports,
+				null_ls.builtins.formatting.clang_format
 			},
 			vim.keymap.set("n", "<leader>fr", function()
 				vim.lsp.buf.format({ async = true })
