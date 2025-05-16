8c534f26371bf78e2b0008686a4156d30847bd03
astrid <96649398+chance-nb@users.noreply.github.com>
2025-05-16 11:36:12 +0200

    tmux: some more tweaking

--- fish/dot-config/fish/functions/edit.fish
+++ fish/dot-config/fish/functions/edit.fish
@@ -4,9 +4,9 @@ set -l session_name "edit "(basename (pwd))
 if tmux has-session -t "$session_name" >/dev/null 2>&1;
     tmux attach -t "$session_name";
 else;
-    tmux new-session -s "$session_name" -n "nvim" nvim \; \
-    new-window -n "testing" \; \
-    new-window -n "git" lazygit \; \
-    select-window -t "nvim";
+    tmux new-session -d -s "$session_name" -n "nvim" nvim
+    tmux new-window -t "$session_name:" -n "testing"
+    tmux new-window -t "$session_name:" -n "git" lazygit
+    tmux attach -t "$session_name:nvim" >/dev/null 2>&1; or tmux switch -t "$session_name:nvim"
 end
 end
