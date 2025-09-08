c9113845b195873c1818906b3afa7194bd39cfa7
chance-nb <96649398+chance-nb@users.noreply.github.com>
2025-09-08 23:43:57 +0200

    fish: update; tmux: update

--- fish/dot-config/fish/functions/edit.fish
+++ fish/dot-config/fish/functions/edit.fish
@@ -4,9 +4,9 @@ set -l session_name "edit "(basename (pwd))
 if tmux has-session -t "$session_name" >/dev/null 2>&1;
     tmux attach -t "$session_name";
 else;
-    tmux new-session -d -s "$session_name" -n "nvim" 'printf \'\033]2;nvim\033\'; nvim'
+    tmux new-session -d -s "$session_name" -n "nvim" nvim --
     tmux new-window -t "$session_name:" -n "testing"
-    tmux new-window -t "$session_name:" -n "git" 'printf \'\033]2;lazygit\033\'; lazygit'
+    tmux new-window -t "$session_name:" -n "git" lazygit --
     tmux attach -t "$session_name:nvim" >/dev/null 2>&1; or tmux switch -t "$session_name:nvim"
 end
 end
